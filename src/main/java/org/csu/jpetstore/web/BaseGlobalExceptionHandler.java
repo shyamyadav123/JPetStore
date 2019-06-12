132c06bdf1d58d5fe2e28198d2ecb02f88c803e5
Nevercome <sunss1903@163.com>
2019-06-12 18:13:49 +0800

    定义GlobalExceptionHandler

---
+++ src/main/java/org/csu/jpetstore/web/BaseGlobalExceptionHandler.java
@@ -0,0 +1,68 @@
+package org.csu.jpetstore.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import java.net.BindException;
+
+/**
+ * 全局异常处理基础类
+ * @author: sun
+ * @date: 2019/6/12
+ */
+@Slf4j
+public class BaseGlobalExceptionHandler {
+
+    /**
+     * 违反约束异常
+     *
+     * @param e 参数验证约束异常
+     */
+    protected DefaultErrorResult handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+        log.info("handleConstraintViolationException start, uri:{}, caused by: ", request.getRequestURI(), e);
+        return DefaultErrorResult.failure(ResultCode.PARAM_IS_INVALID, e, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 处理验证参数封装错误时异常
+     * @param e
+     * @return
+     */
+    protected DefaultErrorResult handleConstraintViolationException(HttpMessageNotReadableException e, HttpServletRequest request) {
+        log.info("handleConstraintViolationException start, uri:{}, caused by: ", request.getRequestURI(), e);
+        return DefaultErrorResult.failure(ResultCode.PARAM_IS_INVALID, e, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 参数绑定异常 （反400错误码）
+     */
+    protected DefaultErrorResult handleBindException(BindException e, HttpServletRequest request) {
+        log.info("handleBindException start, uri:{}, caused by: ", request.getRequestURI(), e);
+        return DefaultErrorResult.failure(ResultCode.PARAM_IS_INVALID, e, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 处理使用@Validated注解时，参数验证错误异常（反400错误码）
+     */
+    protected DefaultErrorResult handleMethodArgumentNotValidExceptionn(MethodArgumentNotValidException e, HttpServletRequest request) {
+        log.info("handleMethodArgumentNotValidException start, uri:{}, caused by: ", request.getRequestURI(), e);
+        return DefaultErrorResult.failure(ResultCode.PARAM_IS_INVALID, e, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 处理通用自定义业务逻辑异常
+     */
+
+    /**
+     * 处理运行时系统异常 500
+     */
+    protected DefaultErrorResult handleRuntimeException(RuntimeException e, HttpServletRequest request) {
+        log.info("handleRuntimeException start, uri:{}, caused by: ", request.getRequestURI(), e);
+        return DefaultErrorResult.failure(ResultCode.SYSTEM_INNER_ERROR, e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/org/csu/jpetstore/web/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package org.csu.jpetstore.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import java.net.BindException;
+
+/**
+ * 全局错误异常处理器
+ * @author: sun
+ * @date: 2019/6/12
+ */
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler extends BaseGlobalExceptionHandler{
+
+    /* 处理400类异常 */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public DefaultErrorResult handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+        return super.handleConstraintViolationException(e, request);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public DefaultErrorResult handleConstraintViolationException(HttpMessageNotReadableException e, HttpServletRequest request) {
+        return super.handleConstraintViolationException(e, request);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public DefaultErrorResult handleMethodArgumentNotValidException(BindException e, HttpServletRequest request) {
+        return super.handleBindException(e, request);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public DefaultErrorResult handleBindException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        return super.handleMethodArgumentNotValidExceptionn(e, request);
+    }
+
+    /* 处理500类异常 */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public DefaultErrorResult handleBindException(RuntimeException e, HttpServletRequest request) {
+        return super.handleRuntimeException(e, request);
+    }
+
+    /* 处理自定义异常 */
+
+}
