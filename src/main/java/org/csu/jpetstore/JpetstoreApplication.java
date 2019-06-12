cbb4fd137d80293974c5ec79471aa701416d02bb
Nevercome <sunss1903@163.com>
2019-06-12 16:35:55 +0800

    init

---
+++ src/main/java/org/csu/jpetstore/JpetstoreApplication.java
@@ -0,0 +1,13 @@
+package org.csu.jpetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpetstoreApplication.class, args);
+    }
+
+}
