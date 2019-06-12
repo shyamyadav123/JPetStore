4074a8def21a696a73f4ae01bc011c632adb368e
Nevercome <sunss1903@163.com>
2019-06-12 09:57:35 +0800

    category

---
+++ src/main/webapp/static/js/common/common.js
@@ -0,0 +1,12 @@
+function getQueryString(name) {
+    const  reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    const r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+
+$(document).ready({
+
+});
