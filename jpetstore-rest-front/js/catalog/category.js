4e41a5d2c011785820261b769e6b3ae6c15bca82
Nevercome <sunss1903@163.com>
2019-06-12 21:28:18 +0800

    add static file

---
+++ jpetstore-rest-front/js/catalog/category.js
@@ -0,0 +1,43 @@
+$(function () {
+    //先从地址栏获取CategoryId
+    const categoryId = getQueryString("categoryId");
+
+    //第一次ajax请求
+    $.ajax({
+        url: "http://localhost:8080/catalog/category/" + categoryId,
+        type: "get",
+        //data用于接收服务器给予数据
+        success: function (data) {
+            //输出查看是否正确
+            console.log("Hello Category");
+            console.log(data);
+            $("#categoryName").text(data.name);
+        },
+        error: function () {
+            alert("Error")
+            //类似java异常处理
+        }
+    });
+    // alert(categoryId);
+
+    let dataHtml = "";
+    //第二次ajax请求
+    $.ajax({
+        url: "http://localhost:8080/catalog/category/" + categoryId + "/products",
+        dataType: 'json',
+        type: "get",
+        success: function (data) {
+            console.log("Hello Products");
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                dataHtml = "<tr><td><a href='../../views/catalog/Product.html?productId=" 
+                + data[i].productId + "&categoryId=" + categoryId + "'>" +
+                    "</a></td><td>" + data[i].name + "</td></tr>";
+                $("#productList").append(dataHtml);
+            }
+        },
+        error: function () {
+            alert("Error")
+        }
+    });
+});
---
+++ jpetstore-rest-front/js/common/common.js
@@ -0,0 +1,53 @@
+function getQueryString(name) {
+    const  reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    const r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
+
+// search
+$(document).ready(function () {
+    $("#searchInput").on("input propertychange", function (e) {
+        const keyword = $(this).val();
+        if (keyword) {
+            $.ajax({
+                url: "catalog/ajax_search",
+                type: "get",
+                data: {
+                    keyword: keyword
+                },
+                success: function (res) {
+                    if (res !== "[]") {
+                        res = res.substring(1, res.length - 1);
+                        let resArr = res.split(", ");
+                        setItems(resArr)
+                    } else {
+                        $("#searchItem").hide();
+                    }
+                }
+            })
+        } else {
+            $("#searchItem").hide();
+        }
+
+    }).blur(function () {
+        //防止失去焦点隐藏 和 点击事件 冲突
+        setTimeout(function () {
+            $("#searchItem").hide();
+        }, 100)
+    });
+});
+
+function setItems(a) {
+    $("#itemList").html("");
+    for (var i in a) {
+        $("#itemList").append("<li class=\"list-group-item\" onmouseover=\"selectItem(this)\" >" + a[i] + "</li>");
+        $("#searchItem").show();
+    }
+}
+
+function selectItem(target) {
+    $("#searchInput").val(target.innerText);
+}
