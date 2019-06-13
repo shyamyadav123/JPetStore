0791dcc279fa375a2ed7f92b6ae4aaa58ed031a9
Nevercome <sunss1903@163.com>
2019-06-13 08:55:17 +0800

    editAccount

---
+++ jpetstore-rest-front/js/account/editAccount.js
@@ -0,0 +1,88 @@
+$(function () {
+    let message = getQueryString('message');
+    if (message == undefined) {
+        message = '';
+    }
+    $('#editAccountMessage').text(message);
+    const auth = getAuthorization();
+    // 未登录 重定向至登陆界面
+    if (auth == undefined || auth == '') {
+        $('#redirect').attr('href', 'SignonForm.html');
+        document.getElementById('redirect').click();
+    } else {
+        // 请求用户信息
+        getUserInfo();
+    }
+});
+
+$(document).ready(function () {
+    $('#editAccount').click(function () {
+        const pwd = $('#password').val();
+        const rePwd = $('#repeatedPassword').val();
+        if (pwd != rePwd) {
+            $('#editAccountMessage').text("password and repeatedPassword is not equal");
+            return;
+        }
+        $('#editAccountForm').attr('action', 'http://localhost:8080/account/' + $('#username').text());
+        $('#editAccountForm').attr('method', 'put');
+        $('#editAccountForm').submit();
+        getUserInfo();
+        // $.ajax({
+        //     url: "http://localhost:8080/account/" + $('#username').text(),
+        //     type: "put",
+        //     dataType: 'json',
+        //     data: {},
+        //     success: function (res) {
+        //         if (res.code == 1) {
+        //             renderPage(res.data)
+        //         } else {
+        //             $('#editAccountMessage').text(res.msg);
+        //         }
+        //     },
+        //     error: function (res) {
+        //         $('#editAccountMessage').text(res.msg);
+        //     }
+        // })
+    })
+});
+
+function getUserInfo() {
+    $.ajax({
+        url: "http://localhost:8080/account/" + getUserId(),
+        type: "get",
+        success: function (res) {
+            if (res.code == 1) {
+                const data = res.data;
+                // 渲染界面
+                renderPage(data);
+            } else {
+                $('#redirect').attr('href', 'SignonForm.html');
+                document.getElementById('redirect').click();
+            }
+        },
+        error: function () {
+            $('#redirect').attr('href', 'SignonForm.html');
+            document.getElementById('redirect').click();
+        },
+        beforeSend: function (request) {
+            request.setRequestHeader("Authorization", auth);
+        }
+
+    })
+}
+
+function renderPage(data) {
+    $('#username').text(data.username);
+    $('#firstName').val(data.firstName);
+    $('#lastName').val(data.lastName);
+    $('#phone').val(data.phone);
+    $('#address1').val(data.address1);
+    $('#address2').val(data.address2);
+    $('#city').val(data.city);
+    $('#zip').val(data.zip);
+    $('#country').val(data.country);
+    $('#languagePreference').val(data.languagePreference);
+    $('#favouriteCategoryId').val(data.favouriteCategoryId);
+    $('#listOption').attr('checked', data.listOption);
+    $('#bannerOption').attr('checked', data.bannerOption);
+}
--- src/main/java/org/csu/jpetstore/controller/AccountController.java
+++ src/main/java/org/csu/jpetstore/controller/AccountController.java
@@ -1,9 +1,12 @@
 package org.csu.jpetstore.controller;
 
+import org.csu.jpetstore.common.result.PlatformResult;
 import org.csu.jpetstore.common.result.ResponseResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.csu.jpetstore.common.security.Authorization;
+import org.csu.jpetstore.domain.Account;
+import org.csu.jpetstore.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: sun
@@ -15,6 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
 
+    @Authorization
+    @GetMapping("/{id}")
+    public PlatformResult getAccount(@PathVariable("id") String id) {
+        return PlatformResult.success(accountService.getAccount(id));
+    }
+
+    @Authorization
+    @PutMapping("/{id}")
+    public PlatformResult updateAccount(@PathVariable("id") String id, @RequestBody Account account) {
+        accountService.updateAccount(account);
+        return PlatformResult.success(accountService.getAccount(account.getUsername()));
+    }
 
 }
